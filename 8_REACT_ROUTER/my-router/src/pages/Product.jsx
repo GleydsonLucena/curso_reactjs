69e4f1f0a1bdc818e3afc2891d6ecb5aef892b3e
Gleydson Lucena <gleydsonlucena3@gmail.com>
2024-08-05 17:50:35 -0300

    feat: Povoamento da página de detalhes com o hook

--- 8_REACT_ROUTER/my-router/src/pages/Product.jsx
+++ 8_REACT_ROUTER/my-router/src/pages/Product.jsx
@@ -1,5 +1,24 @@
+import { useParams } from "react-router-dom";
+import { useFetch } from "../hooks/useFetch";
+
+import "../assets/styles/CardProduct.sass";
+
 const Product = () => {
-  return <div>Product</div>;
+  const { id } = useParams();
+  const url = `http://localhost:3000/products/${id}`;
+  const { products: product, loading, error } = useFetch(url);
+  const { name, price } = product;
+
+  return (
+    <div className="product-container">
+      {error && <div>error.message</div>}
+      {loading && <div>Carregando...</div>}
+      <h1>{name}</h1>
+      <p>
+        O preço de {name} é R$: {price}
+      </p>
+    </div>
+  );
 };
 
 export default Product;
