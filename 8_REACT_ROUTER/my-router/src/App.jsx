2a12345bf252d23ff4095a1aefe3a6bb5924c005
Gleydson Lucena <gleydsonlucena3@gmail.com>
2024-08-05 17:22:53 -0300

    ..

--- 8_REACT_ROUTER/my-router/src/App.jsx
+++ 8_REACT_ROUTER/my-router/src/App.jsx
@@ -1,7 +1,10 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Home from "./pages/Home";
 import About from "./pages/About";
 import NavBar from "./components/NavBar";
+import Product from "./pages/Product";
+
 function App() {
   return (
     <>
@@ -10,6 +13,7 @@ function App() {
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route exact path="/about" element={<About />} />
+          <Route exact path="/products/:id" element={<Product />} />
         </Routes>
       </BrowserRouter>
     </>
--- 8_REACT_ROUTER/my-router/src/components/ProductCard.jsx
+++ 8_REACT_ROUTER/my-router/src/components/ProductCard.jsx
@@ -1,15 +1,21 @@
 import PropTypes from "prop-types";
+
+import { Link } from "react-router-dom";
+
 import "../assets/styles/CardProduct.sass";
 
 const ProductCard = (props) => {
   return (
     <div className="card-product">
       <h2>{props.name}</h2>
-      <p>Preço: {props.price}</p>
+      <p>Preço: R$ {props.price}</p>
       <div className="btn-card">
         <button onClick={() => props.handleDelete(props.id)}>Remover</button>
         <button>Editar</button>
       </div>
+      <Link to={`/products/${props.id}`}>
+        <button>Detalhes</button>
+      </Link>
     </div>
   );
 };
