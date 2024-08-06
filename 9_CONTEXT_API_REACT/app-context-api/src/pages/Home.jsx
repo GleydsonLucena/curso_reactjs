52e194968859fe7065a721c5223a97fdc5da48bd
Gleydson Lucena <gleydsonlucena3@gmail.com>
2024-08-06 18:37:48 -0300

    feat: refatoraçao de contexto para hook

--- 9_CONTEXT_API_REACT/app-context-api/src/pages/Home.jsx
+++ 9_CONTEXT_API_REACT/app-context-api/src/pages/Home.jsx
@@ -1,14 +1,15 @@
-import { useContext } from "react";
-import { CounterContext } from "../context/CounterContext";
+// import { useContext } from "react";
+// import { CounterContext } from "../context/CounterContext";
+import { useCounterContext } from "../hooks/useCounterContext";
 
 const Home = () => {
-  const { counter, setCounter } = useContext(CounterContext);
+  const { counter, setCounter } = useCounterContext();
   return (
     <div className="home">
       <h1>Minha Home</h1>
       <p>Contador: {counter}</p>
       <button onClick={() => setCounter(counter + 1)}>Aidicionar</button>
-      <button onClick={() => setCounter(0)}>Zerar</button>
+      <button>Zerar</button>
     </div>
   );
 };
